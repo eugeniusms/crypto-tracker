51f828da5f56eed4070397d40ee3dddc51ca7736
eugeniusms <eugeniusms@gmail.com>
2022-04-10 06:03:45 +0700

    🔨 Add Basic Styling Row

--- src/App.js
+++ src/App.js
@@ -43,6 +43,8 @@ function App() {
             symbol={coin.symbol}
             volume={coin.market_cap}
             price={coin.current_price} 
+            priceChange={coin.price_change_percentage_24h}
+            marketcap={coin.total_volume}
           />
         )
       })}
